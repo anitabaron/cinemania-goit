413c061bae6cdb7e748754ba1eb6cb09c15a5231
anitabaron <111062951+anitabaron@users.noreply.github.com>
2024-07-26 09:39:21 +0200

    fetch works

--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -1,38 +1,76 @@
+import axios from 'axios';
+
 const API_CODE =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ODIxMjdlZDk3MmU1NmY2YmI3MGFlNzQzZDIzYzFkNyIsIm5iZiI6MTcyMTkyMzEwNy4xMDMyODUsInN1YiI6IjY2YTI3NDZiN2E4ZDcyYzUwNmU3ZmNhMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Qj3afYe7mGMES9vNIydDi4ZdZ8w_kD3c9hqJXKkUUIM';
 const API_KEY = '682127ed972e56f6bb70ae743d23c1d7';
 
-const url = 'https://api.themoviedb.org/3';
+const BASE_URL = 'https://api.themoviedb.org/3';
 
 const params = {
   page: 1,
   language: 'en-US',
+  api_key: API_KEY,
   // include_adult: false,
   // include_video: false,
   // sort_by: 'popularity.desc'
 };
-// https://api.themoviedb.org/3/movie/550?api_key=682127ed972e56f6bb70ae743d23c1d7 //przykładowe zapytanie
+// https://api.themoviedb.org/3/movie/550?api_key=682127ed972e56f6bb70ae743d23c1d7 //przykładowe zapytanie całość ?
 
-const trendMoviesWeek = url + '/trending/movie/week?' + params;
-const trendMoviesDay = url + '/trending/movie/day?' + params;
-const newMovies = url + '/movie/upcoming?' + params;
-const searchMovies = url + '/search/movie?' + params;
-const moreAboutMovie = url + '/movie/movie_id?' + params; // movie_id trzeba wtłoczyć
-const trailerMovie = url + '/movie/movie_id/videos?' + params; // movie_id trzeba wtłoczyć
-const genreMovie = url + '/genre/movie/list?' + params;
-const gcountryMovie = url + '/configuration/countries?' + params;
+const trendMoviesWeek = `${BASE_URL}/trending/movie/week`;
+const trendMoviesDay = `${BASE_URL}/trending/movie/day`;
+const newMovies = `${BASE_URL}/movie/upcoming`;
+const searchMovies = `${BASE_URL}/search/movie`;
+const moreAboutMovie = `${BASE_URL}/movie/{movie_id}`; // movie_id trzeba wtłoczyć
+const trailerMovie = `${BASE_URL}/movie/{movie_id}/videos`; // movie_id trzeba wtłoczyć
+const genreMovie = `${BASE_URL}/genre/movie/list`;
+const gcountryMovie = `${BASE_URL}/configuration/countries`;
+
+const draftMovieList = document.querySelector('.fetch-api__movie-draft');
 
 const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ODIxMjdlZDk3MmU1NmY2YmI3MGFlNzQzZDIzYzFkNyIsIm5iZiI6MTcyMTkyNDE3MS44NDc0OTQsInN1YiI6IjY2YTI3NDZiN2E4ZDcyYzUwNmU3ZmNhMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.K1Qus-92Q3-9KvUxKtanYETfnKoQwXPTlFSSlwqsfFo',
+    Authorization: `Bearer ${API_KEY}`,
   },
 };
 
-fetch(newMovies, options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .then(original_title => console.log(original_title))
-  .catch(err => console.error(err));
+async function fetchMovies(url, options) {
+  try {
+    const response = await axios.get(url, { params, ...options });
+    if (response.data.results) {
+      const movies = response.data.results;
+      displayMovies(movies);
+      return movies;
+    }
+  } catch (error) {
+    console.error(error);
+    throw new Error(`Error! status: ${error.response.status}`);
+  }
+}
+
+function displayMovies(movies) {
+  const moviesMarkup = movies.map(makeMovieItem).join('');
+  draftMovieList.insertAdjacentHTML('beforeend', moviesMarkup);
+}
+
+function makeMovieItem({
+  poster_path,
+  original_title,
+  overview,
+  genre_ids,
+  vote_average,
+}) {
+  return `<li class="fetch-api__movie-draft-item">
+    <a class="fetch-api__movie-draft-item-img" href="${BASE_URL}${poster_path}">
+      <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${original_title}" />
+    </a>
+    <h2>Title: ${original_title}</h2>
+    <p>Descr: ${overview}</p>
+    <p>Genre id: ${genre_ids}</p>
+    <p>Vote: ${vote_average}</p>
+  </li>`;
+}
+
+// Example usage
+fetchMovies(newMovies, options);
