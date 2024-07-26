4770500b579995efd29e957741e09ed1f672c3d7
KrystianRyczek <153994194+KrysianRyczek@users.noreply.github.com>
2024-07-26 23:20:42 +0200

    home page root
    Prepare home page root

---
+++ src/js/index.js
@@ -0,0 +1,41 @@
+import './js/header';
+import './js/hero';
+import './js/movie-list';
+import './js/upcoming';
+import './js/footer';
+import './js/catalog';
+import './js/my-library';
+import './js/modal';
+import './js/oops';
+import './js/buttons-icons';
+import './js/fetch-api';
+
+
+
+
+
+// const createImage = ({ Props })=>{
+//     const li= document.createElement("li")
+//     li.classList.add("movie-trend-itemt")
+//     li.innerHTML=
+//                    `<img class="movie-trend-image"
+// 				        src="${ Props }"
+
+// 				        alt="${ Props }"
+// 				    />
+//                     <div class="trends-movi-description">
+// 						<h3>${ Props }</h3>
+//                         <div class="trends-movi-details">
+// 						    <p>${ Props }</p>
+// 						    <p>${ Props }</p>
+// 					    </div>
+// 					</div>
+// 					<ul class="trends-rateing-list">
+// 						<li>*</li>
+// 						<li>*</li>
+// 						<li>*</li>
+// 						<li>*</li>
+// 						<li>*</li>
+// 					</ul>`
+//     return li
+// }
