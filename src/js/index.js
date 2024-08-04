d1ade2a1584bb729d37eb52af1f7abbf766433c3
KrystianRyczek <153994194+KrystianRyczek@users.noreply.github.com>
2024-08-04 12:42:05 +0200

    scripting
    scripting

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,7 @@
 import './header';
 import './hero';
 import './movie-list';
-import './upcoming';
-import './footer';
 import './my-library';
 import './modal';
-import './oops';
-import './buttons-icons';
 import './api-home';
 import './api-catalog-page';
