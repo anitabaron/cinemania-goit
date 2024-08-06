8dbd87f5a8f9e79e155e989237ecb2c08d2abba5
anitabaron <111062951+anitabaron@users.noreply.github.com>
2024-08-06 22:42:28 +0200

    loader fix

--- src/js/loader.js
+++ src/js/loader.js
@@ -3,7 +3,7 @@ const loader = document.querySelector('#loader');
 loader.classList.add('show-loader');
 
 window.addEventListener('load', function () {
-	setTimeout(function(){
-    	loader.classList.remove('show-loader');
-	}, 2000);
+  setTimeout(function () {
+    loader.classList.remove('show-loader');
+  }, 2000);
 });
