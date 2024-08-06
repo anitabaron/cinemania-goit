aa5fdf3a61cc915e7a2dc7c25a9cc1f9a2e93adb
KamilAminGoIt <158079851+KamilAminGoIt@users.noreply.github.com>
2024-08-06 22:01:54 +0200

    Updated load more btn

--- src/js/my-library.js
+++ src/js/my-library.js
@@ -1,16 +1,9 @@
 import renderMoviesCards from './cards-rendering';
 import LibraryAPI from './library-functions';
 
-// ----------Dorota-----------------
 const btnLoad = document.querySelector('#mylibraryLoadMore');
 let allCards = [];
 let step = 0;
-
-const loadMoreCards = (arr, num) => {
-  const arrOfCards = arr.slice(0, num + 9);
-  renderMoviesCards(arrOfCards, '.my-lib__gallery-list');
-  step += 9;
-};
 
 const library = new LibraryAPI();
 library.setLibrary();
@@ -26,27 +19,33 @@ export function checkStorage() {
     document.querySelector('.my-lib__no-movies').classList.remove('is-hidden');
     document.querySelector('.my-lib__gallery-list').innerHTML = '';
   } else {
-    // ----------Dorota-----------------
     allCards = library.movies;
     const cardPerPage = 9;
     if (allCards.length <= cardPerPage) {
-      btnLoad.classList.toggle('invisible');
       renderMoviesCards(allCards, '.my-lib__gallery-list');
     } else if (!document.querySelector('#mylibraryLoadMore')) {
       return;
     } else if (allCards.length > cardPerPage) {
       const partOfCards = allCards.slice(0, step + 9);
       renderMoviesCards(partOfCards, '.my-lib__gallery-list');
-
       btnLoad.classList.remove('invisible');
       step += 9;
     }
   }
 }
 
+const loadMoreCards = (arr, num) => {
+  const arrOfCards = arr.slice(0, num + 9);
+  renderMoviesCards(arrOfCards, '.my-lib__gallery-list');
+  step += 9;
+};
+
 const loadHandleClick = e => {
   if (e.target === btnLoad) {
     loadMoreCards(allCards, step);
+    if (allCards.length <= step) {
+      btnLoad.classList.add('invisible');
+    }
   }
 };
 
