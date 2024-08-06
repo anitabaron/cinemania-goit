8bb787283d4c5c2f62b8a0978ff42ea795624537
Dorota Dobija <117933735+DorotaDobija@users.noreply.github.com>
2024-08-06 18:37:26 +0200

    Listener works

--- src/js/my-library.js
+++ src/js/my-library.js
@@ -1,20 +1,20 @@
 import renderMoviesCards from './cards-rendering';
 import LibraryAPI from './library-functions';
 
-// const btnLoad = document.querySelector("#mylibraryLoadMore");
-// const cardPerPage = 9;
-// let allCards = [];
-// let step = 0;
+const btnLoad = document.querySelector("#mylibraryLoadMore");
+const cardPerPage = 9;
+let allCards = [];
+let step = 0;
 
-// const loadMoreCards = (arr, num) => {
-//   let end = num + 9; 
-//   if (end < allCards.length) {
-//     end = allCards.length;
-//    };
-//   const arrOfCards = arr.slice(0, end);
-//   renderMoviesCards(arrOfCards, '.my-lib__gallery-list');
-//   step += 9;
-// }
+const loadMoreCards = (arr, num) => {
+  let end = num + 9; 
+  if (end < allCards.length) {
+    end = allCards.length;
+   };
+  const arrOfCards = arr.slice(0, end);
+  renderMoviesCards(arrOfCards, '.my-lib__gallery-list');
+  step += 9;
+}
 
 const library = new LibraryAPI();
 library.setLibrary();
@@ -26,28 +26,30 @@ export function checkStorage() {
     if (!document.querySelector('.my-lib__no-movies')) {
       return;
     }
-    document.querySelector('.my-lib__no-movies').classList.remove('hidden');
+    document.querySelector('.my-lib__no-movies').classList.remove('.hidden');
     document.querySelector('.my-lib__gallery-list').innerHTML = '';
   } else {
     allCards = library.movies;
-    // if (allCards.length <= cardPerPage) {
+    if (allCards.length <= cardPerPage) {
       renderMoviesCards(allCards, '.my-lib__gallery-list');
-    // } else if (allCards.length > cardPerPage) {
-    //   const partOfCards = allCards.slice(0, step + 9);
-    //   renderMoviesCards(partOfCards, '.my-lib__gallery-list');
-    //   btnLoad.classList.remove("invisible");
-    //   step += 9;
-    // }
+    } else if (allCards.length > cardPerPage) {
+      const partOfCards = allCards.slice(0, step + 9);
+      renderMoviesCards(partOfCards, '.my-lib__gallery-list');
+      // btnLoad.classList.remove("invisible");
+      step += 9;
+    }
     
   }
 } 
 
-// console.log(allCards);
-// console.log(step)
+console.log(allCards);
+console.log(step)
 
-// const loadHandleClick = () => {
+const loadHandleClick = (e) => {
+  if (e.target === btnLoad) {
+     loadMoreCards(allCards, step);
+  }
  
-//   loadMoreCards(allCards, step);
-// }
+}
 
-// btnLoad.addEventListener("click", loadHandleClick);
+window.addEventListener("click", loadHandleClick);
