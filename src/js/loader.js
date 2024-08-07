035554360b8b63d03c6a1d19c3dd23028430bf2c
KamilAminGoIt <158079851+KamilAminGoIt@users.noreply.github.com>
2024-08-07 16:31:15 +0200

    Fixed loader

--- src/js/loader.js
+++
@@ -1,9 +0,0 @@
-const loader = document.querySelector('#loader');
-
-loader.classList.add('show-loader');
-
-window.addEventListener('load', function () {
-  setTimeout(function () {
-    loader.classList.remove('show-loader');
-  }, 2000);
-});
